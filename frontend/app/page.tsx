9e8e911a6562fb02fdc0a79358ecd3b7216bd626
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-03-13 21:49:37 +0530

    feat(ui): decent UI implemented

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import Chat from "@/src/components/Chat";
 import Members from "@/src/components/Members";
@@ -11,7 +10,6 @@ import { useEffect } from "react";
 export default function Home() {
   const connect = useConnectionStore((state) => state.connect);
   const disconnect = useConnectionStore((state) => state.disconnect);
-  const join = useConnectionStore((state) => state.join);
   const isConnectedToRoom = useConnectionStore(
     (state) => state.isConnectedToRoom,
   );
@@ -21,12 +19,9 @@ export default function Home() {
     return () => disconnect();
   }, []);
   return (
-    <main className=" w-full min-h-screen flex items-center">
-      <div className="max-w-7xl w-7xl mx-auto h-[90vh] bg-black flex flex-col md:flex-row rounded-2xl p-5 gap-5 border-2 border-white/70">
-        <div className="w-full md:w-1/4 h-fit md:h-full">
-          <Button className="px-4 py-2 rounded" onClick={() => join("general")}>
-            JOIN
-          </Button>
+    <main className="bg-primary w-full min-h-screen flex items-center">
+      <div className="max-w-[1400px] w-[98vw]  mx-auto h-[96vh] bg-primary flex flex-col md:flex-row rounded-[15px] p-5 gap-5 shadow-[0px_0px_100px_1px_#000000]">
+        <div className="w-full md:w-1/4 h-fit rounded-[10px] shadow-[0px_0px_8px_0.5px_#000000] overflow-hidden">
           <Card>
             <CardContent>
               <Members />
@@ -39,13 +34,10 @@ export default function Home() {
           </Card>
         </div>
 
-        <Card className="w-full md:w-3/4 h-full rounded-2xl ">
+        <Card className="w-full md:w-3/4 h-full rounded-[10px] shadow-[0px_0px_8px_0.5px_#000000] overflow-hidden">
           <CardContent>{isConnectedToRoom ? <Chat /> : <></>}</CardContent>
         </Card>
       </div>
     </main>
   );
 }
-
-// backdrop-filter: blur(12px);
-// background: color-mix(in oklch, var(--card) 90%, transparent);
